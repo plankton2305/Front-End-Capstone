5a33588077fc7fb3132cd6b30116f2e4f6d38d99
Kevin Glidden <gliddensolutions@gmail.com>
2023-06-22 11:29:21 -0500

    dependencies set up

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+const App = () => {
+  return (
+    <>
+      Hello World
+    </>
+  )
+}
+
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
