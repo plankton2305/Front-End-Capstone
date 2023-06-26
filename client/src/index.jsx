60111fac1eeb2ed5022a408ade74c4c0ce7a42e0
Kevin Glidden <gliddensolutions@gmail.com>
2023-06-26 18:25:13 -0500

    Added initial request to /products

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import Api from './api/index.js';
 
 const App = () => {
+  const [product_id, setProductId] = useState('');
+  const api = new Api('products');
+
+  useEffect(() => {
+    api.get().then((response) => {
+      setProductId(response.data[0].id);
+    })
+    .catch((err) => {
+      console.log('err', err);
+    })
+  }, [])
+
   return (
     <>
       Hello World!
