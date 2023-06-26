ba711808546277ec00670469f2447c279e101bca
Victor Dai <vdsayf@gmail.com>
2023-06-27 05:59:52 +0800

    Test merge test commit

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import TOKEN from '../config.js'
 
 class Api {
 
--- client/src/components/products/overview/DummyIndex.jsx
+++ client/src/components/products/overview/DummyIndex.jsx
@@ -1,10 +1,22 @@
 import {React, useState, useEffect} from 'react';
 import { createRoot } from 'react-dom/client';
 import Overview from './components/products/overview/Overview.jsx';
-
+import Api from '../../api/index.js';
 
 const App = () => {
   const [currId, setCurrId] = useState(11); //this is dummy data
+
+  const dingus = new Api('products');
+
+  console.log('dingus is:', dingus);
+
+  dingus.get('')
+    .then((data)=>{
+      console.log(data);
+    }).catch((err)=>{
+      console.log('errored')
+      console.log(err)
+    })
 
   return (
     <div>
