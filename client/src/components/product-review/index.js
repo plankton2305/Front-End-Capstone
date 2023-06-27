d4423e6c82e47b2428b5927437dd0af8c7994cc5
April Castaneda <dylanmcastaneda@gmail.com>
2023-06-27 18:04:55 -0500

    add folder for review components and build logic for rendering stars

---
+++ client/src/components/product-review/index.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+import Ratings from './Ratings.jsx';
+import Reviews from './Reviews.jsx';
+
+const Review = () => {
+
+
+  return (
+    // Someting For Ratings
+    // Something For Reviews
+    <section>
+      <Ratings />
+      <Reviews />
+    </section>
+  );
+}
+
+export default Review;
