95b51e0144567daadc2287711297631843954aa7
Kayla Loizzo <kaylaloizzo@gmail.com>
2023-06-28 16:56:13 -0400

    Committing in order to integrate.

--- client/src/components/related-products/RelatedCards.jsx
+++ client/src/components/related-products/RelatedCards.jsx
@@ -9,6 +9,11 @@ import {
 } from "@material-tailwind/react";
 
 
+{/* <Checkbox className='absolute top-0 right-0 m-2'></Checkbox> */}
+{/* <button class='absolute top-0 right-0 m-2'>
+<span class="mask mask-star-2 bg-orange-400"></span>
+</button> */}
+
 const RelatedCards = ({product}) => {
 
   return (
@@ -17,19 +22,27 @@ const RelatedCards = ({product}) => {
       <Card className="mt-6 w-96">
         <CardHeader color="blue-gray" className="relative h-56">
           {product.styles && product.styles.photos && product.styles.photos[0].url ? (
-            <img
-              src={product.styles.photos[0].url}
-              alt={"Product Preview"}
-              layout={"fill"}
-              className={"h-full w-full object-cover"}
+            <div class='relative'>
+              <img
+                class='w-full'
+                src={product.styles.photos[0].url}
+                alt={"Product Preview"}
+                layout={"fill"}
+                className={"h-full w-full object-cover"}
               />
-              ) : (
-                <img
-                  src='https://lh6.googleusercontent.com/K_LvL2XGZdOhq7m-xM6eywUFwM0mLNeE22p05p6T45ejcKLjrJaDHZrxlF5eQ9FtVux4cWNdiIQVJ__6rhhpEqrpLEBjA8o2Fut39URQb1ZxwX687vDzp2xl6VD0E0ghIw=w1280'
-                  alt={"Product Preview"}
-                  layout={"fill"}
-                  className={"h-full w-full object-cover"}
-                />
+              <button class='absolute top-0 right-0 m-2'>button</button>
+            </div>
+            ) : (
+            <div class='realtive'>
+              <img
+                class='w-full'
+                src='https://lh6.googleusercontent.com/K_LvL2XGZdOhq7m-xM6eywUFwM0mLNeE22p05p6T45ejcKLjrJaDHZrxlF5eQ9FtVux4cWNdiIQVJ__6rhhpEqrpLEBjA8o2Fut39URQb1ZxwX687vDzp2xl6VD0E0ghIw=w1280'
+                alt={"Product Preview"}
+                layout={"fill"}
+                className={"h-full w-full object-cover"}
+              />
+              <button class='absolute top-0 right-0 m-2'>button</button>
+            </div>
           )}
         </CardHeader>
         <CardBody>
