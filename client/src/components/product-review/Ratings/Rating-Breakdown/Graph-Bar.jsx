bf65b9ac5129cc5dc479457743e89575d24dd53a
April Castaneda <dylanmcastaneda@gmail.com>
2023-06-30 14:07:48 -0500

    change sizing of Graph Bar fro Rating Distribution Graph

--- client/src/components/product-review/Ratings/Rating-Breakdown/Graph-Bar.jsx
+++ client/src/components/product-review/Ratings/Rating-Breakdown/Graph-Bar.jsx
@@ -2,10 +2,9 @@
 //TODO: Add onClick and hover event handlers
 const GraphBar = ({shadedVal}) => {
   // Bar with a fixed length, calculate amount shaded in based off of total % of ratings
-  console.log(`SHADEDVAL::::: ${shadedVal}`);
   return (
-    <div className="w-80 h-4 bg-[#d4d4d4]">
-      <div className={`bg-[#ffa41c] h-full`} style={{width: shadedVal}}></div>
+    <div className="w-60 h-5 bg-[#d4d4d4] rounded">
+      <div className={`bg-[#ffa41c] h-full rounded`} style={{width: shadedVal}}></div>
     </div>
   )
 }
