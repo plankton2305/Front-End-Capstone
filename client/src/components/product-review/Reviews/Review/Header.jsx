a0fc0a085e7549189acb90252d1e97273658bb05
April Castaneda <dylanmcastaneda@gmail.com>
2023-07-07 11:07:50 -0500

    format date on review component

--- client/src/components/product-review/Reviews/Review/Header.jsx
+++ client/src/components/product-review/Reviews/Review/Header.jsx
@@ -1,11 +1,18 @@
 import { renderStars } from '../../Ratings/Star-Rating.jsx';
 
+const formatDate = (dateString) => {
+  const config = { month: 'long', day: 'numeric', year: 'numeric' };
+  const date = new Date(dateString).toLocaleDateString('en-US', config);
+  console.log(`DATE:::::::::${date}`);
+  return date === 'Invalid Date' ? 'Unknown' : date;
+};
+
 const Header = ({reviewerName, rating, date}) => {
   return (
     <div>
       <span className="mx-[10px]">{renderStars(rating)}</span>
       <span className="mx-[10px]">{reviewerName}</span>
-      <span className="mx-[10px]">{date}</span>
+      <span className="mx-[10px]">{formatDate(date)}</span>
     </div>
   );
 };
